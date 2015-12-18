d663815ef3677fae2e5e4c73f533f3ba65761070
Giang Truong <giang.nguyentruong@vndirect.com.vn>
2015-12-18 13:45:10 +0700

    refactor Tab controller

--- simulator/app/TabController.js
+++ simulator/app/TabController.js
@@ -1,18 +1,53 @@
 TabController = function($scope) {
-    $scope.currentTab = 'app/components/exchange/exchange.html';
+    $scope.currentTab = 'app/components/exchange/monitor.html';
 
-    $scope.showDock = function() {
-        if ($scope.currentTab.indexOf('orderbook') > -1 || $scope.currentTab.indexOf('priceboard') > -1) {
-            $scope.isShowDock = true;
-        } else {
-            $scope.isShowDock = false;
-        }
+    $scope.onClickTabRobot = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/robot/robot.html';
     }
-    
-    $scope.showDock();
 
-    $scope.onClickTab = function (tab) {
-        $scope.currentTab = tab;
-        $scope.showDock();
+    $scope.onClickTabPriceBoard = function () {
+        $scope.isShowDock = true;
+        $scope.currentTab = 'app/components/priceboard/priceboard.html';
+    }
+
+    $scope.onClickTabOrderBook = function () {
+        $scope.isShowDock = true;
+        $scope.currentTab = 'app/components/orderbook/orderbook.html';
+    }
+
+    $scope.onClickTabExchangeMonitor = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/exchange/monitor.html';
+    }
+
+    $scope.onClickTabExchangeOrders = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/exchange/orders.html';
+    } 
+
+    $scope.onClickTabExchangeSecInfo = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/exchange/secinfo.html';
+    } 
+
+    $scope.onClickTabORSBasket = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/basket/basket.html';
+    } 
+
+    $scope.onClickTabORSAfType = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/aftype/aftype.html';
+    }
+
+    $scope.onClickTabORSSecInfo = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/secinfo/secinfo.html';
+    }
+
+    $scope.onClickTabORSAccount = function () {
+        $scope.isShowDock = false;
+        $scope.currentTab = 'app/components/account/account.html';
     }
 } 
