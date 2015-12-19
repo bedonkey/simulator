7b02327db370dff2087d5b825fbd219e3cfebd3e
Giang Truong <giang.nguyentruong@vndirect.com.vn>
2015-12-19 17:03:48 +0700

    Exchagne maintain secinfo itself

---
+++ simulator/app/modules/exchange/InitExData.js
@@ -0,0 +1,72 @@
+InitExData = {
+	secs : [{
+		    id:1,
+		    symbol:"AAA",
+		    floor:1000,
+		    ref: 2000,
+		    ceil: 3000
+		},
+		{
+		    id:2,
+		    symbol:"BBB",
+		    floor:2000,
+		    ref: 3000,
+		    ceil: 4000
+		},
+		{
+		    id:3,
+		    symbol:"CCC",
+		    floor:3000,
+		    ref: 4000,
+		    ceil: 5000
+		},
+		{
+		    id:4,
+		    symbol:"DDD",
+		    floor:4000,
+		    ref: 5000,
+		    ceil: 6000
+		},
+		{
+		    id:5,
+		    symbol:"EEE",
+		    floor:5000,
+		    ref: 6000,
+		    ceil: 7000
+		},
+		{
+		    id:6,
+		    symbol:"FFF",
+		    floor:6000,
+		    ref: 7000,
+		    ceil: 8000
+		},
+		{
+		    id:7,
+		    symbol:"GGG",
+		    floor:7000,
+		    ref: 8000,
+		    ceil: 9000
+		},
+		{
+		    id:8,
+		    symbol:"HHH",
+		    floor:8000,
+		    ref: 9000,
+		    ceil: 10000
+		},
+		{
+		    id:9,
+		    symbol:"III",
+		    floor:9000,
+		    ref: 10000,
+		    ceil: 11000
+		},
+		{
+		    id:10,
+		    symbol:"KKK",
+		    floor:10000,
+		    ref: 11000,
+		    ceil: 12000
+		}]
+}
