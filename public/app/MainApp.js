697bd1f64def569c63970fec640ec771d99c3235
bedonkey <bedonkey@github.com>
2016-08-15 23:46:03 +0700

    View lesson explain

--- public/app/MainApp.js
+++ public/app/MainApp.js
@@ -54,3 +54,21 @@ app.directive("basketAdd", function(){return {templateUrl:'app/modules/ors/baske
 app.directive("mask", function(){return {templateUrl:'app/shared/mask.html'};});
 
 app.filter('split', function() {return function(input, splitChar, splitIndex) {return input.split(splitChar)[splitIndex];}});
+app.filter('to_trusted', ['$sce', function($sce){
+    return function(text) {
+        return $sce.trustAsHtml(text);
+    };
+}]);
+app.directive('compileTemplate', function($compile, $parse){
+        return {
+            link: function(scope, element, attr){
+                var parsed = $parse(attr.ngBindHtml);
+                function getStringValue() { return (parsed(scope) || '').toString(); }
+
+                //Recompile if the template changes
+                scope.$watch(getStringValue, function() {
+                    $compile(element, null, -9999)(scope);  //The -9999 makes it skip directives so that we do not recompile ourselves
+                });
+            }
+        }
+    })
