f55dcf0034920b04a98fa85c69eba6b48d1b95dd
bedonkey <bedonkey@github.com>
2016-08-16 08:33:57 +0700

    Hot fix translator

--- public/app/modules/lesson/TestcaseTranslator.js
+++ public/app/modules/lesson/TestcaseTranslator.js
@@ -4,7 +4,6 @@ TestCaseTranslator = function() {
 TestCaseTranslator.prototype = {
     translate: function(input) {
         if (input.indexOf('#')) {
-            console.log("Translate this:" + input)
             return input.split('#')[1];
         }
     }
